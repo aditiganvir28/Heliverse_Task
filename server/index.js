40efff25b829339a9ff9839cc47f0af64b9f247a
aditiganvir28 <100085991+aditiganvir28@users.noreply.github.com>
2023-12-09 01:58:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT
 
 app.use(
   cors({
-    origin: "*",
+    origin: [process.env.frontend],
     methods: ['GET', 'POST', 'UPDATE', 'PUT'],
     credentials: true,
   }),
